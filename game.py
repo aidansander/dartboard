8fe05f028327e16ea4eb414e0c6111720ad531ea
Aidan Sander <aidan.sander@gmail.com>
2021-08-01 21:09:57 +0000

    Updated Game class docs. fixed tests. support frame=none in Game.

--- game.py
+++ game.py
@@ -15,21 +15,27 @@ class Player:
 
 
 class Game:
+    """Dartboard game class
     
-    def __init__(self, playerlist, frame):
+    Parameters:
+        playerlist: list of Players
+        frame: tkframe to display score. May be None, in which case update does nothing
+    
+    """
+    def __init__(self, playerlist, frame=None):
         self.players = playerlist
-        
+         
         self.dartcounter = 0
         self.playerlabels =[]
         self.frame = frame
         self.playercount = len(self.players)
 
-        tablefont = tkFont.Font(family="Arial",size=28)
-
-        for i in self.players:
-            self.playerlabels.append(tk.Label(frame, text=i.name, bg='white', fg='black', font=tablefont))
-        for i in self.playerlabels:
-            i.pack(side=tk.TOP)
+        if frame:
+            tablefont = tkFont.Font(family="Arial",size=28)
+            for i in self.players:
+                self.playerlabels.append(tk.Label(frame, text=i.name, bg='white', fg='black', font=tablefont))
+            for i in self.playerlabels:
+                i.pack(side=tk.TOP)
 
     def record_dart(self, dart):
         self.players[(self.dartcounter//3)%len(self.players)].record_dart(dart)
@@ -45,12 +51,12 @@ class Game:
         for i in player.hist:
             score += i.multiplier * i.number
         return score
-    
-    
+     
     def update(self):
-        for i in range (0,self.playercount):
-            self.playerlabels[i]['text'] = self.score_player(self.players[i])
-        self.frame.update()
+        if self.frame:
+            for i in range (0,self.playercount):
+                self.playerlabels[i]['text'] = self.score_player(self.players[i])
+            self.frame.update()
 
 class CountDownPlayer (Player):
 
@@ -59,25 +65,57 @@ class CountDownPlayer (Player):
         self.count_down = count_down
         self.double_in = double_in
         self.double_out = double_out
-        self.is_in = False
+        self.reset()
+        self.is_in = not double_in
+        self.is_out = not double_out
+        self.score = count_down
+        self.tempscore = count_down
         super().__init__(name)
-
+    
+    def reset(self):
+        self.is_in = not self.double_in
+        self.is_out = not self.double_out
+    
     def record_dart(self, dart):
         super().record_dart(dart)
-
-
-
-class CountDownGame (Game):
-    def __init__(self, playernames, frame, count_down, double_in, double_out):
-        self.count_down = count_down
-        self.double_in = double_in
-        self.double_out = double_out
+        score_dart(len(self.hist-1))
         
-        playerlist = [CountDownPlayer(i, count_down, double_in, double_out) for i in playernames]
+    def score_dart(dartindex):
+        dart = self.hist[dartindex]
+        if dart.multiplier == 2:
+            self.is_in = True
+            
+        if self.is_in:
+            self.tempscore -= dart.multiplier * dart.number
+        if dartindex % 3 == 2:
+            if self.tempscore > 0:
+                self.score = self.tempscore
+            elif self.tempscore < 0:
+                self.tempscore = self.score
+        if self.tempscore == 0:
+            if self.is_out:
+                self.score = self.tempscore
 
-        super().__init__(playerlist, frame)
+            elif dartindex % 3 ==0 and dart.multiplier == 2:
+                self.score = self.tempscore
+            else:#TODO double check if double out requires double dart to win, i.e. does double 1, single 1, single 1, turn win on 4 points left, or does only double 2 win?
+                self.tempscore = self.score
 
-    def score_player(self, player):
-        score = self.count_down
-        
+
+
+
+
+#class CountDownGame (Game):
+#    def __init__(self, playernames, frame, count_down, double_in, double_out):
+#        self.count_down = count_down
+#        self.double_in = double_in
+#        self.double_out = double_out
+#        
+#        playerlist = [CountDownPlayer(i, count_down, double_in, double_out) for i in playernames]
+#
+#        super().__init__(playerlist, frame)
+#
+#    def score_player(self, player):
+#        score = self.count_down
+#        
 
--- test_game.py
+++ test_game.py
@@ -18,7 +18,6 @@ def test_game_0():
     g.record_dart(Dart(3, 20))
     g.record_dart(Dart(2, 17))
 
-    g.update()
 
     assert g.players[0].hist[0] == Dart(3,20) 
     assert g.players[0].hist[1] == Dart(1,6) 
@@ -36,7 +35,6 @@ def test_game_undo_dart():
     g.undo_dart()
     g.record_dart(Dart(2, 17))
 
-    g.update()
 
     assert g.players[0].hist[0] == Dart(3,20) 
     assert g.players[0].hist[1] == Dart(1,6) 
